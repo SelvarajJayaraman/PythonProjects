2c5179434089d9ceb83f8ddbc7ced48c8d31e517
Selvaraj Jayaraman <techselva87@gmail.com>
2020-05-11 17:56:09 +0530

    Basic Numbers,String,Variables examples

---
+++ PythonBasicExamples/Numbers.py
@@ -0,0 +1,42 @@
+'''
+Created on 11-May-2020
+
+@author: selvaraj
+'''
+#1.Int
+#2.Float
+#3.Complex
+
+print("#2 Python Numbers Example");
+print("*************************");
+
+a = 10;
+b = 30;
+
+c = a+b;
+print("Addition of Int Values:");
+print(type(c));
+print(c);
+print("\n");
+
+bf = 25.30;
+cf = 35.50;
+ef = bf + cf;
+print("Addition of Float Values:");
+print(type(ef));
+print(ef);
+print("\n");
+
+ac = 3+5j;
+bc = 4+6j;
+cc = ac + bc;
+print("Addition of Complex Values:");
+print(type(cc));
+print(cc);
+print("\n");
+
+#Power of 10 values
+d = 3e5;
+print("Power of 10:");
+print(type(d));
+print(d);
---
+++ PythonBasicExamples/Variables.py
@@ -0,0 +1,20 @@
+'''
+Created on 11-May-2020
+
+@author: selvaraj
+'''
+
+print("#1 Python Variables Example");
+print("***************************")
+
+a = 10;
+print(type(a));
+print("\n");
+
+b= "Selva";
+print(type(b));
+print("\n");
+
+c=25.23;
+print(type(c));
+print("\n");
