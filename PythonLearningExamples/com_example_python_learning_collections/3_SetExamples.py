f9356ac6d55471b4c1b7ad9420aaffbe465df052
Selvaraj Jayaraman <techselva87@gmail.com>
2020-05-25 15:24:24 +0530

    Set collection examples

--- PythonLearningExamples/com_example_python_learning_collections/3_SetExamples.py
+++ PythonLearningExamples/com_example_python_learning_collections/3_SetExamples.py
@@ -4,7 +4,7 @@ Created on 25-May-2020
 @author: Selvaraj Jayaraman
 '''
 print("#  Python Set  statement");
-print("*********************");
+print("************************");
 
 a={1,2,3};
 print("A is type:",type(a));
@@ -19,3 +19,32 @@ c.add(1);
 c.add(2);
 c.add(3);
 print("After adding set collection values:",c);
+
+c.remove(2);
+print("After using remove method:",c);
+
+c.pop();
+print("After pop method:",c);
+
+#mixed collection in set
+d={(1,2),(3,4)};
+print("Mixed Set collection:",d);
+
+#Java- DeepCopy
+e={1,2,3,4};
+f=e;
+print("Deep copy of set collection:",e);
+print("Deep copy of set collection:",f);
+
+e.add(5);
+f.add(6);
+print("Deep copy of set collection:",e);
+print("Deep copy of set collection:",f);
+
+#ShallowCopy
+g= e.copy();
+print("Shallow copy of set collection:",g);
+e.add(7);
+print("after adding value in set(e) collection:",e);
+print("after adding value in set(e) and copy(g) collection:",g);
+
